40f5e27aa4b14f895971f47f3683f7b27c2e9ace
pirate4593 <joconnor10@student.cvtc.edu>
2023-02-12 21:09:48 -0600

    Prototype design

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config() // Read environment variables from .env
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5163
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function (req, res) {
+    res.render('pages/index')
+  })
+
+  .get('/about', (req, res) => {
+    res.render('pages/about')
+  })
+
+  .get('/health', (req, res) => {
+    res.status(200).send('Healthy')
+  })
+
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
